6841759759535031ac2c173068eb970f271dd2c3
Nikolay <example@gmail.com>
2021-12-20 21:09:24 +0200

    v2.0

--- Lesson_1/js/script.js
+++ Lesson_1/js/script.js
@@ -20,4 +20,20 @@ btns[0].addEventListener('click', () => {
     btns[1].classList.toggle('red');
 });
 
-console.log(btns[0].className);
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.classList.contains('red')) {
+        console.log('Hello');
+    }
+});
+
+// btns.forEach( btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello!');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
